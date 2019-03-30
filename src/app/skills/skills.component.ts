7bb54ad7e2f404b52c23fa1bdbd7b1e539bc9719
robertLichtnow <matheusrobert8@gmail.com>
2019-03-30 16:10:18 -0300

    Fixed skills screen

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -50,12 +50,10 @@ export class SkillsComponent implements OnInit, OnDestroy {
   get mySoftSkills():Skills{
     if(this._mySoftSkills == null ){
       this._mySoftSkills = new Skills();
-      this._mySoftSkills.skills.set("Self-Motivation",6);
+      this._mySoftSkills.skills.set("Motivation",6);
       this._mySoftSkills.skills.set("Problem Solving",5);
-      this._mySoftSkills.skills.set("Communication",4);    
-      this._mySoftSkills.skills.set("Leadership",3);
+      this._mySoftSkills.skills.set("Communication",4); 
       this._mySoftSkills.skills.set("Negotiation", 2);
-      this._mySoftSkills.skills.set("Work Under Pressure",4);
       this._mySoftSkills.skills.set("Flexibility", 6);
       this._mySoftSkills.skills.set("Team Work", 4);
       this._mySoftSkills.skills.set("Open Mind", 6);
@@ -71,13 +69,13 @@ export class SkillsComponent implements OnInit, OnDestroy {
   get myHardSkills():Skills{
     if(this._myHardSkills == null ){
       this._myHardSkills = new Skills();
-      this._myHardSkills.skills.set("Angular",6);
+      this._myHardSkills.skills.set("Angular",5);
       this._myHardSkills.skills.set("JS/TS",5);
       this._myHardSkills.skills.set("Java",5);
       this._myHardSkills.skills.set("Git",4);    
       this._myHardSkills.skills.set("Docker", 2);
       this._myHardSkills.skills.set("PHP",3);
-      this._myHardSkills.skills.set("Jira/Trello", 5);
+      this._myHardSkills.skills.set("Jira/Trello", 6);
       this._myHardSkills.skills.set("Scrum",4);
       this._myHardSkills.skills.set("Bootstrap 4", 5);
       this._myHardSkills.skills.set("CSS", 4);
