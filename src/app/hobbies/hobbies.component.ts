efa57c4561c708891bd19dab5b4acb775000f4de
robertLichtnow <matheusrobert8@gmail.com>
2019-04-01 23:38:51 -0300

    Remaking hobbies screen

--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -8,6 +8,8 @@ import {
 } from '@angular/animations';
 import { AnimationStates } from '../animation-states';
 import { wait } from '../utils/utils';
+import { Hobbies } from './hobbies';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-hobbies',
@@ -40,6 +42,22 @@ export class HobbiesComponent implements OnInit {
 
   animationState: AnimationStates = AnimationStates.RIGHT;
 
+  _hobbies:Hobbies[] = [];
+
+  get hobbies():Hobbies[]{
+    if(!this._hobbies.length){
+      let h = this._hobbies;
+      h.push(new Hobbies("Gaming", 10,"#0FA3B1"));
+      h.push(new Hobbies("Anime", 15,"#7B4B94"));
+      h.push(new Hobbies("Pubs", 4, "#EDDEA4"));
+      h.push(new Hobbies("Music",6,"#F71735"));
+      h.push(new Hobbies("Procrastinating", 2, "#FF9B42"));
+    }
+    return this._hobbies;
+  }
+
+  hobbiesChart = [];
+
   get date():Date{
     return new Date();
   }
@@ -49,6 +67,37 @@ export class HobbiesComponent implements OnInit {
   ngOnInit() {
     wait(0).subscribe(() => {
       this.animationState = AnimationStates.MIDDLE;
+    });
+
+    this.loadHobbiesChart();
+  }
+
+  private loadHobbiesChart(){
+    this.hobbiesChart = new Chart('hobbies-canvas', {
+      type: 'doughnut',
+      data: {
+        labels: this.hobbies.map(v => v.name),
+        datasets: [
+          {
+            label: "Soft Skills",
+            data: this.hobbies.map(v => v.value),
+            backgroundColor: this.hobbies.map(v => v.color)
+          }
+        ]
+      },
+      options:{
+        legend:{
+          position: 'right',
+          labels:{
+            fontSize: 14,
+            padding: 8,
+            fontColor: '#FFF'
+          }
+        },
+        animation: {
+          easing: 'easeInOutCubic'
+        }
+      }
     });
   }
 
---
+++ src/app/hobbies/hobbies.ts
@@ -0,0 +1,13 @@
+export class Hobbies {
+    name:string;
+    value:number;
+    color:string;
+
+    constructor(name:string, 
+        value:number,
+        color:string){ 
+            this.name = name;
+            this.value = value;
+            this.color = color;
+        }
+}
